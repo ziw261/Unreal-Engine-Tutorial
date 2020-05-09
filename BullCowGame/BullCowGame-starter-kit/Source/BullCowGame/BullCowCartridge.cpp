4fce3266c4aaa04892dc589f504d2a2d71a147e4
ziw261 <ziw261@ucsd.edu>
2020-05-08 21:41:59 -0700

    Minor Update
    FString instead of string
    Use TEXT to embed the string

--- BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
-{
+void UBullCowCartridge::BeginPlay() { // When the game starts 
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
+void UBullCowCartridge::OnInput(const FString& Input) { // When the player hits enter
+
+    ClearScreen();
+
+    FString HiddenWord = TEXT("cake");
 
 }
+
